771a1bd4f7d1fbf0d2c8c2e40397737e18849606
Olivier PEREZ <olivier@olivierperez.fr>
2019-09-16 04:39:52 +0200

    WIP

---
+++ kotlin/snippets/Snippet-005.kt
@@ -0,0 +1,26 @@
+package fr.o80.kata
+
+fun maMéthode1(arg1: Int) {
+    if(arg1 == ...){
+        val arg2 = // TODO OPZ Vérifier si l'utilisateur est connecté
+        maMéthode2(arg2)
+    } else {
+        println(message)
+    }
+}
+
+fun maMéthode2(arg: Int) {
+    if(arg == ...){
+        // logiqueMethode2
+    } else {
+        println(message)
+    }
+}
+
+fun maMéthode3(arg: Int) {
+    if(arg == ...){
+        // logiqueMethode3
+    } else {
+        println(message)
+    }
+}
