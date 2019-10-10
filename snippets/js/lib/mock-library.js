abc1a9d118b49f71b37555e0e10780ba944ddd05
Mathieu Mure <mathieu.mure@gmail.com>
2019-10-10 20:05:03 +0200

    feat(js): implements snippet-004 with javascript

---
+++ snippets/js/lib/mock-library.js
@@ -0,0 +1,20 @@
+export class MainScreen {}
+
+export class AccountsScreen {}
+
+export const iconsBlock = {
+    setNotification(a, b) {}
+}
+
+export const Accounts = {
+    getInstance () {
+        return Accounts;
+    },
+
+    getAllAccountsList (callback) {},
+
+    GetAccountsCalback: class GetAccountsCalback {
+        onSuccess(){}
+        onFailure(){}
+    },
+}
