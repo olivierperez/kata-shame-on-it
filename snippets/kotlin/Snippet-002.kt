657c37395c6c5d3a893bbcc0d71db32066c9157f
Christophe Pagnoux-Vieuxfort <christophe.pagnoux@abbeal.com>
2019-11-19 21:06:45 +0100

    Snippet 2

--- snippets/kotlin/Snippet-002.kt
+++ snippets/kotlin/Snippet-002.kt
@@ -1,18 +1,15 @@
 data class Product(
     val type: Int,
     val price: Float
-)
+) {
+    val foodType = 19
 
-/**
- * Get the products that are about food.
- */
-fun get(list: List<Product>): Int {
-    val ret = mutableListOf<Product>()
-    for (x in list) {
-        if (x.type != 19) {
-            continue
-        }
-        ret.add(x)
-    }
-    return ret.size
+    fun isFood(): Boolean = this.type == foodType
+}
+
+
+fun getNumberOfProductThatAreAboutFood(list: List<Product>): Int {
+    return list
+        .filter { it.isFood() }
+        .size
 }
